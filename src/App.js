2afbde20eb17bd02485d3f743d6ba8492ef7ae47
Dante0214 <rn_chic@naver.com>
2025-10-19 15:37:29 +0900

    feat: 초기 입력 및 빈 문자열 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  async run() {}
-}
+import { Console } from "@woowacourse/mission-utils";
 
+class App {
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      if (input.trim() === "") {
+        throw new Error("[ERROR] 빈 값이 입력되었습니다.");
+      }
+      Console.print(`문자열 : ${input}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
+}
 export default App;
